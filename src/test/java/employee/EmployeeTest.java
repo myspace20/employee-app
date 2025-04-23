bc37135393fe4f8191ad8f4bbcacac72c53e1915
Roger Satsi <rogersatsi98@gmail.com>
2025-04-23 16:29:00 +0000

    test:added a test case for searching for employees by department

--- src/test/java/employee/EmployeeTest.java
+++ src/test/java/employee/EmployeeTest.java
@@ -1,12 +1,14 @@
 package employee;
 
 import org.employee.employee_app.exceptions.EmployeeNotFound;
+import org.employee.employee_app.exceptions.InvalidDepartment;
 import org.employee.employee_app.models.Employee;
 import org.employee.employee_app.models.EmployeeDB;
 import org.employee.employee_app.models.EmployeeID;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -29,6 +31,30 @@ public class EmployeeTest {
         employeeDatabase.addEmployee(employee1);
         Optional<Employee<EmployeeID>> retrievedEmployee = employeeDatabase.retrieveEmployeeById(employeeID1);
         assertEquals(employee1, retrievedEmployee.get());
+    }
+
+
+    @Test
+    public void getEmployeesByDepartment() throws InvalidDepartment{
+        String department = "IT";
+        EmployeeID employeeID1 = new EmployeeID();
+        EmployeeID employeeID2 = new EmployeeID();
+        EmployeeID employeeID3 = new EmployeeID();
+        Employee<EmployeeID> employee1 = new Employee<>(employeeID1,"Roger",department,2000,4,3,true);
+        Employee<EmployeeID> employee2 = new Employee<>(employeeID2,"Roger",department,2000,4,3,true);
+        Employee<EmployeeID> employee3 = new Employee<>(employeeID3,"Roger",department,2000,4,3,true);
+        employeeDatabase.addEmployee(employee1);
+        employeeDatabase.addEmployee(employee2);
+        employeeDatabase.addEmployee(employee3);
+
+        List <Employee<EmployeeID>> employees = employeeDatabase.getEmployeesByDepartment(department);
+
+        for(Employee<EmployeeID> employee: employees){
+            assertEquals(department, employee.getDepartment());
+        }
+
+
+
     }
 
 
