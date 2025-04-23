7e02a14d67b7f6089921b9e97d11ee971cb33787
Roger Satsi <rogersatsi98@gmail.com>
2025-04-23 16:14:59 +0000

    test:added a test case for adding an employee to the employee database

--- src/test/java/employee/EmployeeTest.java
+++ src/test/java/employee/EmployeeTest.java
@@ -1,8 +1,15 @@
 package employee;
 
+import org.employee.employee_app.exceptions.EmployeeNotFound;
+import org.employee.employee_app.models.Employee;
 import org.employee.employee_app.models.EmployeeDB;
 import org.employee.employee_app.models.EmployeeID;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class EmployeeTest {
@@ -12,6 +19,16 @@ public class EmployeeTest {
     @BeforeAll
     public static void setUp(){
         employeeDatabase = new EmployeeDB<>();
+    }
+
+
+    @Test
+    public void AddEmployeeTest() throws EmployeeNotFound {
+        EmployeeID employeeID1 = new EmployeeID();
+        Employee<EmployeeID> employee1 = new Employee<>(employeeID1,"Roger","IT",2000,4,3,true);
+        employeeDatabase.addEmployee(employee1);
+        Optional<Employee<EmployeeID>> retrievedEmployee = employeeDatabase.retrieveEmployeeById(employeeID1);
+        assertEquals(employee1, retrievedEmployee.get());
     }
 
 
