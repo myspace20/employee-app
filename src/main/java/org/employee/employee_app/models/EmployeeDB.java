2b019ab042efa7cbe219afb9adbd1df096bb372e
Roger Satsi <rogersatsi98@gmail.com>
2025-04-24 17:07:01 +0000

    Add null last to sorting with custom comparators to handle null values

--- src/main/java/org/employee/employee_app/models/EmployeeDB.java
+++ src/main/java/org/employee/employee_app/models/EmployeeDB.java
@@ -117,18 +117,17 @@ public class EmployeeDB<T> {
 
 
     public List<Employee<T>> getTopFiveHighestPaidEmployees(){
-
-        return employeeDB.values().stream().sorted(Employee.EmployeeSalaryComparator).limit(5).toList();
+        return employeeDB.values().stream().sorted(Comparator.nullsLast(Employee.EmployeeSalaryComparator)).limit(5).toList();
     }
 
 
     public List<Employee<T>> sortEmployeesByRating(){
-        return employeeDB.values().stream().sorted(EmployeePerformanceComparator).toList();
+        return employeeDB.values().stream().sorted(Comparator.nullsLast(EmployeePerformanceComparator)).toList();
     }
 
 
     public List<Employee<T>> sortEmployeesSalaryHighestFirst(){
-        return employeeDB.values().stream().sorted(Employee.EmployeeSalaryComparator).toList();
+        return employeeDB.values().stream().sorted(Comparator.nullsLast(Employee.EmployeeSalaryComparator)).toList();
     }
 
 
